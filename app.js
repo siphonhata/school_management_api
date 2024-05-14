410713a171462251b2ca6ecee49c01e477591f6e
siphonhata <siphonhata@gmail.com>
2024-05-15 00:31:43 +0200

    feature: login & create user

--- app.js
+++ app.js
@@ -1,83 +1,86 @@
 // Import required modules
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
-
-// Initialize express app
 const app = express();
-
 const cors = require('cors');
+const bcrypt = require('bcrypt');
 
-const { Pool } = require('pg');
-
-// Create a connection pool
-const pool = new Pool({
-  user: 'postgres',
-  host: '192.168.0.131',
-  database: 'school_management',
-  password: 'postgres',
-  port: 5432, 
-});
-
-// Use bodyParser middleware to parse JSON requests
 app.use(bodyParser.json());
-// Use cors middleware to enable CORS
 app.use(cors());
 
 
-async function query(text, params) {
-    const client = await pool.connect();
-    try {
-      const result = await client.query(text, params);
-      return result.rows;
-    } finally {
-      client.release();
-    }
-  }
+async function hashPassword(password)
+{
+  const saltRounds = 10; // Adjust as needed
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  return hashedPassword;
+} 
+  //CRUD Endpoints
 
-
-  async function getUsers() {
-    const users = await query('SELECT * FROM users');
-    console.log(users);
-  }
-  
-  // Connect to the database and execute a query
-  pool.connect((err, client, done) => {
-    if (err) {
-      console.error('Error connecting to PostgreSQL database', err);
-      return;
+  // Endpoint to create a new user
+app.post('/users', async (req, res) => 
+  {
+    try 
+    {
+      const 
+      {
+        first_name, last_name, id_number, date_of_birth, gender,
+        email, phone_number, address, role, password,
+      } = req.body;
+      //Hashing the password using salt
+      const hashedPassword = await hashPassword(password)
+      const result = await prisma.users.create({data: {first_name, last_name, id_number, date_of_birth, gender, email, phone_number, address, role, password: hashedPassword}})
+      
+      res.status(201).json(result);
+    } 
+    catch (error) 
+    {
+      console.error('Error creating user:', error);
+      res.status(500).send('Internal Server Error');
     }
-    console.log('Connected to PostgreSQL database');
-  
-    // Example query
-    getUsers();
-  
-    // Release the client back to the pool
-    done();
   });
-  
-// Mock user data (Replace this with a real user database)
-const users = [
-    { email: 'siphonhata@gmail.com', password: '123' },
-    { email: 'user2@example.com', password: 'password2' }
-];
 
 // Route to handle login requests
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => 
+  {
     const { email, password } = req.body;
-    console.log(req.body)
-    // Check if user exists
-    const user = users.find(u => u.email === email && u.password === password);
-    if (user) {
-        // Redirect or respond with success message
-        res.json({ message: 'Login successful', user });
-    } else {
-        // Respond with error message for incorrect credentials
-        res.status(401).json({ message: 'Invalid email or password' });
+    
+    // 1. Validate request body
+    if (!email || !password) 
+    {
+      return res.status(400).json({ error: 'Missing email or password' });
+    }
+  // 2. Find user by email
+    try 
+    {
+      const user = await prisma.users.findUnique({ where: { email } });
+      if (!user) 
+      {
+        return res.status(401).json({ error: 'User does not exist' });
+      }
+
+    // 3. Compare password hashes
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) 
+      {
+        return res.status(401).json({ error: 'Invalid password' });
+      }
+
+    // 4. Login successful (generate session or token here, optional)
+      return res.json({ message: 'Login successful', success: true });
+    } 
+    catch (error) 
+    {
+      console.error(error);
+      return res.status(500).json({ error: 'Internal server error' });
     }
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.APP_PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
---
+++ prisma/migrations/20240514215840_anything/migration.sql
@@ -0,0 +1,133 @@
+-- CreateEnum
+CREATE TYPE "user_role" AS ENUM ('STUDENT', 'TEACHER', 'PARENT', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "classroom" (
+    "id" TEXT NOT NULL,
+    "class_number" INTEGER,
+    "grade" VARCHAR(10),
+
+    CONSTRAINT "classroom_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "extracurricular_activity" (
+    "id" TEXT NOT NULL,
+    "activity_name" VARCHAR(255),
+    "teacher" VARCHAR(255),
+    "description" TEXT,
+    "start_date" DATE,
+    "end_date" DATE,
+
+    CONSTRAINT "extracurricular_activity_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "parent" (
+    "id" TEXT NOT NULL,
+    "userid" VARCHAR(36),
+    "studentid" VARCHAR(36),
+
+    CONSTRAINT "parent_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "password_reset_tokens" (
+    "id" VARCHAR(35) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "token" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "password_reset_tokens_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "students" (
+    "id" TEXT NOT NULL,
+    "userid" VARCHAR(36),
+    "subjects" VARCHAR(255),
+    "extracurricular_activity" VARCHAR(255),
+    "classroomid" VARCHAR(36),
+
+    CONSTRAINT "students_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "subject" (
+    "id" TEXT NOT NULL,
+    "subject_name" VARCHAR(255),
+
+    CONSTRAINT "subject_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "teacher" (
+    "id" TEXT NOT NULL,
+    "userid" VARCHAR(36),
+    "classroomid" VARCHAR(36),
+    "subjectid" VARCHAR(36),
+    "timetableid" VARCHAR(36),
+
+    CONSTRAINT "teacher_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "timetable" (
+    "id" TEXT NOT NULL,
+    "subjectid" VARCHAR(36),
+    "starttime" TIME(6),
+    "endtime" TIME(6),
+    "day" VARCHAR(20),
+    "date" DATE,
+    "type" VARCHAR(20),
+    "teacherid" VARCHAR(36),
+    "classroomid" VARCHAR(36),
+
+    CONSTRAINT "timetable_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "first_name" VARCHAR(255) NOT NULL,
+    "last_name" VARCHAR(255) NOT NULL,
+    "id_number" VARCHAR(255) NOT NULL,
+    "date_of_birth" VARCHAR(10),
+    "gender" VARCHAR(10),
+    "email" VARCHAR(255) NOT NULL,
+    "phone_number" VARCHAR(20) NOT NULL,
+    "address" VARCHAR(255),
+    "role" "user_role" NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "parent" ADD CONSTRAINT "parent_studentid_fkey" FOREIGN KEY ("studentid") REFERENCES "students"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "parent" ADD CONSTRAINT "parent_userid_fkey" FOREIGN KEY ("userid") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "students" ADD CONSTRAINT "students_classroomid_fkey" FOREIGN KEY ("classroomid") REFERENCES "classroom"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "students" ADD CONSTRAINT "students_userid_fkey" FOREIGN KEY ("userid") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "teacher" ADD CONSTRAINT "teacher_classroomid_fkey" FOREIGN KEY ("classroomid") REFERENCES "classroom"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "teacher" ADD CONSTRAINT "teacher_subjectid_fkey" FOREIGN KEY ("subjectid") REFERENCES "subject"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "teacher" ADD CONSTRAINT "teacher_userid_fkey" FOREIGN KEY ("userid") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "timetable" ADD CONSTRAINT "timetable_classroomid_fkey" FOREIGN KEY ("classroomid") REFERENCES "classroom"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "timetable" ADD CONSTRAINT "timetable_subjectid_fkey" FOREIGN KEY ("subjectid") REFERENCES "subject"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "timetable" ADD CONSTRAINT "timetable_teacherid_fkey" FOREIGN KEY ("teacherid") REFERENCES "teacher"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
