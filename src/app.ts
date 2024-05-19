b268164f9b3f35703d02ce1ba2ea51b3c5c7b1ea
Dineo Mathibela <dineomathibela161@gmail.com>
2024-05-19 20:37:22 +0200

    fix: change to typescript

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,19 @@
-require('dotenv').config()
-const express = require('express');
-const bodyParser = require('body-parser');
+import 'dotenv/config';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import auth from './auth.js'; // Update the path if necessary
+
 const app = express();
-const cors = require('cors');
-const auth  = require('./auth');
 
 app.use(bodyParser.json());
 app.use(cors());
 
 app.use('/', auth);
+
 // Start the server
 const PORT = process.env.APP_PORT || 3000;
 
-app.listen(PORT, () => 
-{
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
